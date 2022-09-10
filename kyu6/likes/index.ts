94bbecfbb2281e3a8a7ffe4632de639521560d78
gabriellukke <gabriellukke@outlook.com>
2022-09-10 20:12:46 -0300

    feat: first implementation

--- kyu6/likes/index.ts
+++ kyu6/likes/index.ts
@@ -1,5 +1,22 @@
 // Link https://www.codewars.com/kata/5266876b8f4bf2da9b000362/typescript
 
 export function likes(a : string[]) {
-  return '';
+  if (!a.length) return 'no one likes this';
+
+  const [first, second, third] = a;
+  const others = a.length - 2;
+
+  if (a.length > 3) {
+    return `${first}, ${second} and ${others} others like this`;
+  }
+
+  if (a.length === 3) {
+    return `${first}, ${second} and ${third} like this`;
+  }
+
+  if (a.length === 2) {
+    return `${first} and ${second} like this`;
+  }
+
+  return `${first} likes this`;
 }
