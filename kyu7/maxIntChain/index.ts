899c9a5ad5ee601799a16001ade1716737c21277
gabriellukke <gabriellukke@outlook.com>
2022-09-10 19:44:35 -0300

    refact: reduce lines with unary operator

--- kyu7/maxIntChain/index.ts
+++ kyu7/maxIntChain/index.ts
@@ -1,12 +1,8 @@
-// Link: https://www.codewars.com/kata/631082840289bf000e95a334
+// Link: https://www.codewars.com/kata/631082840289bf000e95a334/typescript
 
 export function maxIntChain(n: number): number {
   if (n < 5) return -1;
   const half = n / 2;
   const isEven = n % 2 === 0;
-
-  if (isEven) {
-    return (half + 1) * (half - 1);
-  }
-  return Math.ceil(half) * Math.floor(half);
+  return isEven ? (half + 1) * (half - 1) : Math.ceil(half) * Math.floor(half);
 }
