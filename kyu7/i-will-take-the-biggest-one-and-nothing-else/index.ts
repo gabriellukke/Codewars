ad55f6e892eb3a9049d4a9a740bd420adf0b3c66
gabriellukke <gabriellukke@outlook.com>
2022-09-10 19:41:03 -0300

    feat: first implementation

--- kyu7/i-will-take-the-biggest-one-and-nothing-else/index.ts
+++ kyu7/i-will-take-the-biggest-one-and-nothing-else/index.ts
@@ -1,5 +1,22 @@
 // Link: https://www.codewars.com/kata/631082840289bf000e95a334
 
 export function maxIntChain(n: number): number {
-  return 0;
+  if (n < 5) return -1;
+
+  let half = 0;
+  let biggest = 0;
+  let chain = 0;
+
+  const isOdd = n % 2 === 1;
+  if (isOdd) {
+    half = n / 2;
+    biggest = Math.ceil(half);
+    chain = biggest - 1;
+
+  } else {
+    half = n / 2;
+    biggest = half + 1;
+    chain = half - 1;
+  }
+  return biggest * chain;
 }
