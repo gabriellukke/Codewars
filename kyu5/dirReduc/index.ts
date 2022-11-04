b329fd7a1d63cdc8ce73895481777b1487a94b5e
Gabriel <gabriel.dev.almeida@outlook.com>
2022-11-04 16:53:40 -0300

    feat: first implementation

--- kyu5/dirReduc/index.ts
+++ kyu5/dirReduc/index.ts
@@ -1,2 +1,19 @@
 export function dirReduc(arr: string[]): string[] {
+  const opposite = {
+    NORTH: 'SOUTH',
+    SOUTH: 'NORTH',
+    EAST: 'WEST',
+    WEST: 'EAST',
+  };
+  
+  const test = arr.reduce((acc, dir) => {
+    if (acc[acc.length - 1] === opposite[dir as keyof typeof opposite]) {
+      acc.pop();
+    } else {
+      acc.push(dir);
+    }
+    return acc;
+  }, [] as string[]);
+
+  return test;
 }
