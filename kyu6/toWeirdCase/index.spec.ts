845dd2f6c4da2fc73977437454d368defd869e9d
Gabriel <gabriel.dev.almeida@outlook.com>
2022-11-04 13:07:59 -0300

    test: spec cases

---
+++ kyu6/toWeirdCase/index.spec.ts
@@ -0,0 +1,12 @@
+import { toWeirdCase } from '.';
+
+describe('toWeirdCase', () => {
+  it('should return the correct value for a single word', () => {
+    expect(toWeirdCase('This')).toBe('ThIs');
+    expect(toWeirdCase('is')).toBe('Is');
+  });
+
+  it('should return the correct value for multiple words', () => {
+    expect(toWeirdCase('This is a test')).toBe('ThIs Is A TeSt');
+  });
+});
---
+++ kyu6/toWeirdCase/index.ts
@@ -0,0 +1,3 @@
+export function toWeirdCase(string: string){
+  //TODO
+}
