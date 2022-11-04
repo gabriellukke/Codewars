387cc34c716a9549b734c6984360786e5bef2822
Gabriel <gabriel.dev.almeida@outlook.com>
2022-11-04 16:24:30 -0300

    feat: first implementation

--- kyu6/toWeirdCase/index.ts
+++ kyu6/toWeirdCase/index.ts
@@ -1,3 +1,19 @@
-export function toWeirdCase(string: string){
-  //TODO
+export function toWeirdCase(string: string) {
+  const words = string.split(' ');
+  let result = '';
+  const odd = (index: number) => index % 2 === 0;
+
+  words.forEach((word, index) => {
+    for (let i = 0; i < word.length; i++) {
+      if (odd(i)) {
+        result += word[i].toUpperCase();
+      } else {
+        result += word[i].toLowerCase();
+      }
+    }
+    if (index !== words.length - 1) {
+      result += ' ';
+    }
+  });
+  return result;
 }
