ed67383b3a17ccdba26be78919f5d8b28f29b241
Gabriel <gabriel.dev.almeida@outlook.com>
2022-11-08 07:02:53 -0300

    feat: first implementation

--- kyu5/romanNumerals/index.ts
+++ kyu5/romanNumerals/index.ts
@@ -1,4 +1,26 @@
 export function romaNumerals(roman: string): number {
-  // complete the solution by transforming the 
-  // string roman numeral into an integer  
+  const romanNumerals = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let result = 0;
+
+  for (let i = 0; i < roman.length; i++) {
+    const current = romanNumerals[roman[i] as keyof typeof romanNumerals];
+    const next = romanNumerals[roman[i + 1] as keyof typeof romanNumerals];
+
+    if (current < next) {
+      result -= current;
+    } else {
+      result += current;
+    }
+  }
+
+  return result;
 }
