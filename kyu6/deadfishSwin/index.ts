e2fd45d0685c579f82440b749c6199c4a0a20cfa
Gabriel <gabriel.dev.almeida@outlook.com>
2022-11-08 11:23:14 -0300

    feat: first implementation

--- kyu6/deadfishSwin/index.ts
+++ kyu6/deadfishSwin/index.ts
@@ -1,3 +1,16 @@
-export function deadfishSwim(str: string): number[] {
-  // Todo
+export function deadfishSwim(data: string): number[] {
+  const result: number[] = [];
+  let value = 0;
+  const commands = {
+    i: () => value++,
+    d: () => value--,
+    s: () => (value *= value),
+    o: () => result.push(value),
+  };
+
+  data.split('').forEach((command) => {
+    const commandFunc = commands[command as keyof typeof commands];
+    commandFunc && commandFunc();
+  });
+  return result;
 }
