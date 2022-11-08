f746fbf50c79d8868195569e4023c36e178180cd
Gabriel <gabriel.dev.almeida@outlook.com>
2022-11-08 06:50:40 -0300

    feat: first implementation

--- kyu5/humanReadable/index.ts
+++ kyu5/humanReadable/index.ts
@@ -1,3 +1,9 @@
 export function humanReadable(seconds:number):string {
-  // TODO
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds - (hours * 3600)) / 60);
+  const secs = seconds - (hours * 3600) - (minutes * 60);
+
+  const zeroFormater = (n:number) => n < 10 ? `0${n}` : n;
+
+  return `${zeroFormater(hours)}:${zeroFormater(minutes)}:${zeroFormater(secs)}`;
 }
