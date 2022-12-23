1e5ab6659b627d671e1a4eea294abb22e91f6a8d
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2022-12-23 01:04:37 -0300

    feat: first implementation

--- kyu5/simplePigLatin/index.ts
+++ kyu5/simplePigLatin/index.ts
@@ -1,4 +1,12 @@
-export function pigIt() {
-  // ...
+export function pigIt(phrase: string): string {
+  const words = phrase.split(' ');
+  const pigLatinWords = words.map((word) => {
+    const firstLetter = word[0];
+    const restOfWord = word.slice(1);
+    return `${restOfWord}${firstLetter}ay`;
+  });
+  const pigLatinPhrase = pigLatinWords.join(' ');
+
+  return pigLatinPhrase;
 }
 
