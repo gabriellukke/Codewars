666e66b98d9fb649e923f6d2bc0d210664e2470d
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2022-12-23 01:11:41 -0300

    test: add new spec case

--- kyu5/simplePigLatin/index.spec.ts
+++ kyu5/simplePigLatin/index.spec.ts
@@ -5,5 +5,9 @@ describe('pigIt', () => {
     expect(pigIt('Pig latin is cool')).toBe('igPay atinlay siay oolcay');
     expect(pigIt('This is my string')).toBe('hisTay siay ymay tringsay');
   });
+
+  it('should leave punctuation marks untouched', () => {
+    expect(pigIt('Hello world !')).toBe('elloHay orldway !');
+  });
 });
 
