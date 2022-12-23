6ead308970ee9c135728438dd0961c6890cff755
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2022-12-23 01:13:42 -0300

    feat: first refact

--- kyu5/simplePigLatin/index.ts
+++ kyu5/simplePigLatin/index.ts
@@ -1,9 +1,12 @@
 export function pigIt(phrase: string): string {
   const words = phrase.split(' ');
   const pigLatinWords = words.map((word) => {
-    const firstLetter = word[0];
-    const restOfWord = word.slice(1);
-    return `${restOfWord}${firstLetter}ay`;
+    if (word.match(/[a-zA-Z]/)) {
+      const firstLetter = word[0];
+      const restOfWord = word.slice(1);
+      return `${restOfWord}${firstLetter}ay`;
+    }
+    return word;
   });
   const pigLatinPhrase = pigLatinWords.join(' ');
 
